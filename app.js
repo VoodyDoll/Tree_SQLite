0ed5cefaf566e11ebcb8ddc24d036094ec769bea
god <atlantis555@mail.ru>
2023-02-07 15:11:44 +0300

    sp0.1

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const { engine } = require ('express-handlebars');
+const { engine } = require ('express-handlebars')
 
-app.set('view engine','handlebars')
+app.set('view engine','hbs')
 
-app.engine('handlebars', engine());
+app.engine('hbs', engine({
+	extname:'hbs',
+	defaultLayout:'index',
+	partialsDir:'./views/partials'
+}));
 
 app.use(express.static('public'))
 
 app.get('/', (req, res)=> {
-  res.render('main',{layout:'index'})
+  res.render('main',{layout:'planB'})
 })
 
 app.listen(port, ()=> {
