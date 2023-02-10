4f31d875e7c03f464598df52fe16eeaf40498db7
god <atlantis555@mail.ru>
2023-02-10 15:09:54 +0300

    sp0.3 good rezult

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const { engine } = require ('express-handlebars')
+let express = require('express')
+let app = express()
+let { engine } = require ('express-handlebars')
+// let bootstrap=require('bootstrap')
+let port = 3000
 let indexRouter = require('./routes/index');
 // let usersRouter = require('./routes/users');
+
+ 
+  
+
+// app.use("/js",express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")))
+  
+  
+
 app.set('view engine','hbs')
 
 app.engine('hbs', engine({
@@ -13,11 +22,11 @@ app.engine('hbs', engine({
 }));
 
 app.use(express.static('public'))
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use("/js",express.static(__dirname + "/node_modules/bootstrap/dist/js"))
 // Подключение роутеров
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-
-
 
 app.listen(port, ()=> {
   console.log('Example app listening on port')
