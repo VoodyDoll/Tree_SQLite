d65badbb0c74acae05e7a25e8c5ff9fe5377b34b
Stas <atlantisscale@gmail.com>
2023-02-12 15:38:30 +0300

    save in db, connect db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
-
+// подключение к mongo
+const MongoClient = require("mongodb").MongoClient;
 
 router.get('/', (req, res)=> {
+    
+const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(url);
+ 
+async function run() {
+    try {
+    	// Подключаемся к серверу
+        await mongoClient.connect();
+        // обращаемся к базе данных begine
+        const db = mongoClient.db("begine");
+         // обращаемся к коллекции people
+        const collection = db.collection("people");
+        // определяем кол-во документов в коллекции
+        const count = await collection.countDocuments();
+
+        console.log(`В коллекции users ${count} документа/ов`);
+    }catch(err) {
+        console.log(err);
+    } finally {
+        await mongoClient.close();
+    }
+}
+run().catch(console.error);
 
 
 let kot=[
