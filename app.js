51160e1718a19cd339e163c23a62b9b3036df7a4
god <atlantis555@mail.ru>
2023-02-14 15:24:01 +0300

    begine

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 let express = require('express')
 let app = express()
 let { engine } = require ('express-handlebars')
+// Подключение SQLite
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+
+  db.run('CREATE TABLE lorem (info TEXT)');
+  var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+
+  for (var i = 0; i < 10; i++) {
+    stmt.run('Ipsum ' + i);
+  }
+
+  stmt.finalize();
+
+  db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
+    console.log(row.id + ': ' + row.info);
+  });
+});
+
+db.close();
+
+
+
+
 // let bootstrap=require('bootstrap')
 let port = 3000
 let indexRouter = require('./routes/index');
