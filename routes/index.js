dc11f8467e2da71467f58367a265e6b3d55c7568
god <atlantis555@mail.ru>
2023-02-16 14:54:06 +0300

    filtres good

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('SQL/roots.db');
 
+let kot=[
+	{id:1,name:'gus',img:'./images/boot1.jpg',description:'Прекрасная стерлядь',cost:'150'},
+	{id:2,name:'pusy',img:'./images/boot2.jpg',description:'Прекрасная вобла',cost:'200'},
+	{id:3,name:'girl',img:'./images/boot3.jpg',description:'Прекрасная куница',cost:'300'},
+	{id:4,name:'girl',img:'./images/boot3.jpg',description:'Прекрасная куница',cost:'300'},
+	{id:5,name:'girl',img:'./images/boot3.jpg',description:'Прекрасная куница',cost:'300'}
 
+]
+
+
+// основная страница
 router.get('/', (req, res)=> {
 
 
@@ -29,26 +39,32 @@ router.get('/', (req, res)=> {
 // db.close();
 // Подключение SQLite
  db.all("SELECT * FROM coffe", (err, row)=> {
-    // console.log(err)
-    // console.log(row)
-
-
-// res.render('main',{layout:'planB',row:row,kot:kot})
-    // res.json(row)          
-         
-
-let kot=[
-	{id:1,name:'gus',img:'./images/boot1.jpg',description:'Прекрасная стерлядь',cost:'150'},
-	{id:2,name:'pusy',img:'./images/boot2.jpg',description:'Прекрасная вобла',cost:'200'},
-	{id:3,name:'girl',img:'./images/boot3.jpg',description:'Прекрасная куница',cost:'300'},
-	{id:4,name:'girl',img:'./images/boot3.jpg',description:'Прекрасная куница',cost:'300'},
-	{id:5,name:'girl',img:'./images/boot3.jpg',description:'Прекрасная куница',cost:'300'}
-
-]
-	
 
   res.render('main',{layout:'planB',row:row,kot:kot})
 })
 })
+
+// фильтр дешовых продуктов
+router.get('/low_cost', (req, res)=> {	
+
+db.all("SELECT * FROM coffe ORDER BY cost ASC", (err, row)=> {
+
+res.render('main',{layout:'planB',row:row,choice_cost:'От дешовых к дорогим'})
+
+})
+})
+
+// фильтр дорогих продуктов
+router.get('/expensive_cost', (req, res)=> {
+	
+
+db.all("SELECT * FROM coffe ORDER BY cost DESC", (err, row)=> {
+
+
+res.render('main',{layout:'planB',row:row,choice_cost:'От дорогих к дешовым'})
+
+})
+})
+
 module.exports = router;
 
