cb47105ffd078a2468d119c071d8c13397e0a87d
Stas <atlantisscale@gmail.com>
2023-02-26 20:14:28 +0300

    very good

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
 let express = require('express')
 let app = express()
 let { engine } = require ('express-handlebars')
+let indexRouter = require('./routes/index');
 let helpers=require('./components/hbsHelpers')
-// Подключение SQLite
-// var sqlite3 = require('sqlite3').verbose();
-// var db = new sqlite3.Database(':memory:');
-
-// db.serialize(function() {
-
-//   db.run('CREATE TABLE lorem (info TEXT)');
-//   var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
-
-//   for (var i = 0; i < 10; i++) {
-//     stmt.run('Ipsum ' + i);
-//   }
-
-//   stmt.finalize();
-
-//   db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-//     console.log(row.id + ': ' + row.info);
-//   });
-// });
-
-// db.close();
 
 app.set('view engine','hbs')
 
@@ -30,12 +10,11 @@ app.engine('hbs', engine({
   extname:'hbs',
   defaultLayout:'index',
   partialsDir:'./views/partials',
-  registerHelper:'./components/hbsHelpers'
+  helpers: require("./components/hbsHelpers") 
 }));
 
 
 let port = 3000
-let indexRouter = require('./routes/index');
 
 app.use(express.static('public'))
 // подключение bootstrup
--- components/hbsHelpers.js
+++ components/hbsHelpers.js
@@ -1,7 +1,35 @@
 let helper={
-	for:(pages)=>{
-
+	for:(add)=>{
+var result = ''
+for (var i = 1; i <= add; i++) {
+    result += i
+  }
+return ('<ul>' + result + '</ul>')
 	}
 }
 
 module.exports=helper;
+
+
+// var register = function(Handlebars) {
+//   var helpers = {
+//     // put all of your helpers inside this object
+//     ifEquals: function(){
+//         return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+//     }
+//   };
+
+//   if (Handlebars && typeof Handlebars.registerHelper === "function") {
+//     // register helpers
+//     for (var prop in helpers) {
+//         Handlebars.registerHelper(prop, helpers[prop]);
+//     }
+//   } else {
+//       // just return helpers object if we can't register helpers here
+//       return helpers;
+//   }
+
+// };
+
+// module.exports.register = register;
+// module.exports.helpers = register(null); 
