1733ea949c9a23676da89b3ea55e00597ed60b64
Stas <atlantisscale@gmail.com>
2023-03-12 16:41:34 +0300

    base pag wiev

--- routes/index.js
+++ routes/index.js
@@ -3,68 +3,93 @@ var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('SQL/roots.db');
 
-// пагинация
-let page=1;
-let limit=8;
-// общее кол-во страниц
-let total_pages=0;
-let all_posts=24;
+let start_index=''
+let end_index=''
+let result_users=''
+let limit=8  
+let total_pages=''
 
 router.get('/',(req, res)=> {  
 
-    // страница от
-    let is_page=Number(limit*req.query.page-limit)
-        // страница до
-        let next_page=is_page+limit      
-if (req.query.page) {
-db.each("SELECT COUNT(*) AS datacount FROM coffe", (err, roww)=> {
+    if (req.query.page) {
 
-        total_pages=Math.ceil(roww.datacount/limit);         
+            db.all(`SELECT * FROM coffe`, (err, row)=> { 
+// Расчет страниц
+                 start_index=(req.query.page-1)*limit
 
-       db.all(`SELECT * FROM coffe WHERE post_id BETWEEN ${is_page} AND ${next_page} LIMIT ${limit}`, (err, row)=> {
+                 end_index=req.query.page*limit
 
-         res.render('main',{layout:'planB',roww:total_pages,row:row})    
-  
-}) 
-})
-}else {
-    // основная страница
+                 result_users=row.slice(start_index, end_index)
 
-    // запросы на данные  
-     db.all(`SELECT * FROM coffe LIMIT ${limit}`, (err, row)=> {  
-    // запросы на пагинацию (общее кол-во старниц)
-    db.each("SELECT COUNT(*) AS datacount FROM coffe", (err, roww)=> {
+                console.log(result_users)
+// общее кол-во стараниц
+                total_pages=Math.ceil(row.length/limit);
 
-    total_pages=Math.ceil(roww.datacount/limit);
+               res.render('main',{layout:'planB',roww:result_users,total_pages:total_pages})    
+               
+           }) 
+        
+    }else {
+// основная страница
 
-      res.render('main',{layout:'planB',roww:total_pages,row:row})
+// запросы на данные  
+       db.all(`SELECT * FROM coffe`, (err, row)=> {  
+// общее кол-во стараниц
+        total_pages=Math.ceil(row.length/limit);
+// кол-во страниц на странице
+         result_users=row.slice(1, limit+1)        
+
+                console.log(row)
+           
+            res.render('main',{layout:'planB',roww:result_users,total_pages:total_pages})       
 
     })
 
-    })
-
-}
+   }
 
 });
 
 // фильтр дешовых продуктов
 router.get('/low_cost', (req, res)=> {	
 
-db.all("SELECT * FROM coffe ORDER BY cost ASC", (err, row)=> {
+is_page=Number(limit*req.query.page-limit)
+        // страница до
+    next_page=is_page+limit    
 
-res.render('main',{layout:'planB',row:row,choice_cost:'От дешовых к дорогим'})
+// if (req.query.page) {
+
+        db.each("SELECT COUNT(*) AS datacount FROM coffe", (err, roww)=> {
+
+            total_pages=Math.ceil(roww.datacount/limit);         
+
+            db.all(`SELECT * FROM coffe WHERE post_id BETWEEN ${is_page} AND ${next_page} LIMIT ${limit}`, (err, row)=> {
+
+               res.render('main',{layout:'planB',roww:total_pages,row:row,choice_cost:'От дешовых к дорогим'})    
+               
+           }) 
+      
+    // }else {
+
+    db.all(`SELECT * FROM coffe ORDER BY cost ASC LIMIT ${limit}`, (err, row)=> {
+
+        res.render('main',{layout:'planB',row:row,choice_cost:'От дешовых к дорогим'})
+
+    })
+// }
 
 })
-})
+
+  })
+
 
 // фильтр дорогих продуктов
 router.get('/expensive_cost', (req, res)=> {	
 
-db.all("SELECT * FROM coffe ORDER BY cost DESC", (err, row)=> {
+    db.all("SELECT * FROM coffe ORDER BY cost DESC", (err, row)=> {
 
-res.render('main',{layout:'planB',row:row,choice_cost:'От дорогих к дешовым'})
+        res.render('main',{layout:'planB',row:row,choice_cost:'От дорогих к дешовым'})
 
-})
+    })
 })
 
 module.exports = router;
