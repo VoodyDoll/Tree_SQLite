e655da0d6ef54ee1d69b4e37245161a6a8bc09e0
Stas <atlantisscale@gmail.com>
2023-04-23 20:20:52 +0300

    good bag

--- public/JS/bag.js
+++ public/JS/bag.js
@@ -1,22 +1,20 @@
+// Создание массива корзины
 let mass_bag=[]
-let jo=''
-let img=''
-let name_product=''
-let cost_product=''
-let id=''
-
-
-
-
-
+let but_id=''
 document.onclick=event=> {
 
 if (event.target.className=='btn btn-primary order') {
 
-img=event.target.closest('.card').firstElementChild.src
-name_product=event.target.closest('.card').children[1].firstElementChild.innerHTML
-cost_product=event.target.closest('.card').children[1].firstElementChild.nextElementSibling.firstElementChild.innerHTML
-id=event.target.dataset.id
+// работа с кнопкой
+event.target.innerHTML='Товар в карзине'
+event.target.setAttribute("disabled", "")
+document.querySelector('.bag').style.color='red'
+
+// получение элементов из формы
+let img=event.target.closest('.card').firstElementChild.src
+let name_product=event.target.closest('.card').children[1].firstElementChild.innerHTML
+let cost_product=event.target.closest('.card').children[1].firstElementChild.nextElementSibling.firstElementChild.innerHTML
+let id=event.target.dataset.id
 
 if (localStorage.getItem('a')!=null) {
 
@@ -27,24 +25,13 @@ mass_bag=JSON.parse(mass_bag)
 mass_bag.push({id:id,img:img,name:name_product,cost:cost_product})
 
 localStorage.setItem('a',JSON.stringify(mass_bag))
-// let b=localStorage.getItem('a')
-// b=JSON.parse(b)
-// console.log(b)
+
 }else{
 
 mass_bag=[{id:id,img:img,name:name_product,cost:cost_product}]
 localStorage.setItem('a',JSON.stringify(mass_bag))
-// let b=localStorage.getItem('a')
-// b=JSON.parse(b)
+
 }
-
-// mass_bag.push(localStorage.getItem('a'))
-
-// получение объектов страницы
-
-
-
-
 
 }
 
