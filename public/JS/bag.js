4dbca2394603ecd0d9d40fca8a0fa1f139b0e715
Stas <atlantisscale@gmail.com>
2023-04-27 18:51:07 +0300

    good

--- public/JS/bag.js
+++ public/JS/bag.js
@@ -1,39 +1,69 @@
 // Создание массива корзины
 let mass_bag=[]
 let but_id=''
-document.onclick=event=> {
 
-if (event.target.className=='btn btn-primary order') {
+let jo=document.querySelectorAll('.btn btn-primary order')
+for(let i of jo){
+  
+  console.log(i)
+  
+}
 
-// работа с кнопкой
-event.target.innerHTML='Товар в карзине'
-event.target.setAttribute("disabled", "")
-document.querySelector('.bag').style.color='red'
-
-// получение элементов из формы
-let img=event.target.closest('.card').firstElementChild.src
-let name_product=event.target.closest('.card').children[1].firstElementChild.innerHTML
-let cost_product=event.target.closest('.card').children[1].firstElementChild.nextElementSibling.firstElementChild.innerHTML
-let id=event.target.dataset.id
 
 if (localStorage.getItem('a')!=null) {
+	mass_bag=localStorage.getItem('a')
+	mass_bag=JSON.parse(mass_bag)
+	// console.log(mass_bag)
+	for (let i of mass_bag) {
 
-mass_bag=localStorage.getItem('a')
 
-mass_bag=JSON.parse(mass_bag)
+		console.log(i.id)
 
-mass_bag.push({id:id,img:img,name:name_product,cost:cost_product})
-
-localStorage.setItem('a',JSON.stringify(mass_bag))
-
-}else{
-
-mass_bag=[{id:id,img:img,name:name_product,cost:cost_product}]
-localStorage.setItem('a',JSON.stringify(mass_bag))
-
+	}
 }
 
-}
+
+
+
+
+document.onclick=event=> {
+
+
+
+
+	if (event.target.className=='btn btn-primary order') {
+
+// работа с кнопкой
+
+		event.target.innerHTML='Товар в карзине'
+		event.target.setAttribute("disabled", "")
+		document.querySelector('.bag').style.color='red'
+
+// получение элементов из формы
+		let img=event.target.closest('.card').firstElementChild.src
+		let name_product=event.target.closest('.card').children[1].firstElementChild.innerHTML
+		let cost_product=event.target.closest('.card').children[1].firstElementChild.nextElementSibling.firstElementChild.innerHTML
+		let id=event.target.dataset.id
+
+// запись в localStorage
+		if (localStorage.getItem('a')!=null) {
+
+			mass_bag=localStorage.getItem('a')
+
+			mass_bag=JSON.parse(mass_bag)
+
+			mass_bag.push({id:id,img:img,name:name_product,cost:cost_product})
+
+			localStorage.setItem('a',JSON.stringify(mass_bag))
+
+		}else{
+
+			mass_bag=[{id:id,img:img,name:name_product,cost:cost_product}]
+			localStorage.setItem('a',JSON.stringify(mass_bag))
+
+		}
+
+	}
 
 
 }
