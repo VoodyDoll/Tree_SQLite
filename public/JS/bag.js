6f349431a005787da660b74d26ed691f38b2b484
Stas <atlantisscale@gmail.com>
2023-06-18 15:15:46 +0300

    goob work button

--- public/JS/bag.js
+++ public/JS/bag.js
@@ -46,24 +46,24 @@ document.onclick=event=> {
 
 	mass_bag=localStorage.getItem('a')
 
-if (mass_bag!=null) {
+	if (mass_bag!=null) {
 
-			mass_bag=JSON.parse(mass_bag)
+		mass_bag=JSON.parse(mass_bag)
 
-			mass_bag.forEach((item) => {
+		mass_bag.forEach((item) => {
 
-				if(item!=null){
+			if(item!=null){
 
 				// item.id=Number(item.id)
 
-					mass_bag.push(item)
-					
+				mass_bag.push(item)
+
 				mass_bag=[...new Set(mass_bag)];
-					
-				}
-			});
+
+			}
+		});
 		
-		}
+	}
 	
 // console.log(mass_bag)
 
@@ -115,11 +115,13 @@ if (mass_bag!=null) {
 		let mass_konf=[]
 		let mass_pipa=[]
 		cot=Number(event.target.dataset.id)
-		let name_produ=event.target.closest('.card').children[2].firstElementChild.innerHTML
-     
-
-		console.log(name_produ)
-console.log(cot)
+		
+// замена 'Товар в карзине' на 'заказать' и разблокировать кнопку
+		let name_produ=event.target.closest('.card').children[2].firstElementChild.nextElementSibling.firstElementChild
+		name_produ.innerHTML='Заказать'
+		name_produ.disabled = false;
+		
+		console.log(cot)
 // В localStorage преобразуем в массив его id
 		mass_bag=localStorage.getItem('a')
 // console.log(mass_bag)
@@ -131,7 +133,7 @@ console.log(cot)
 
 				if(item!=null){
 
-				item.id=Number(item.id)
+					item.id=Number(item.id)
 
 					op.push(item.id)
 
@@ -139,40 +141,39 @@ console.log(cot)
 				}
 			});
 
-		
 		}
-
-console.log(op)
-console.log(op.indexOf(cot))
+document.querySelector('.badge').innerHTML=op.length-1
+		console.log(op)
+		console.log(op.indexOf(cot))
 
 // стирание выбраной позиции оставляет null
 
 		// 
 		delete mass_bag[op.indexOf(cot)]
-delete op[op.indexOf(cot)]
+		delete op[op.indexOf(cot)]
 		// console.log(mass_bag)
 		// console.log(op)
 
-for (let a of mass_bag) {
-	if (a!=null) {
-		mass_konf.push(a)
-		mass_konf=[...new Set(mass_konf)];
-		mass_bag=mass_konf
-mass_bag=[...new Set(mass_bag)];
-	}
-}
+		for (let a of mass_bag) {
+			if (a!=null) {
+				mass_konf.push(a)
+				mass_konf=[...new Set(mass_konf)];
+				mass_bag=mass_konf
+				mass_bag=[...new Set(mass_bag)];
+			}
+		}
 
-for (let ai of op) {
-	if (ai!=null && ai!='') {
-		mass_pipa.push(ai)
-		mass_pipa=[...new Set(mass_pipa)];
-		op=mass_pipa
+		for (let ai of op) {
+			if (ai!=null && ai!='') {
+				mass_pipa.push(ai)
+				mass_pipa=[...new Set(mass_pipa)];
+				op=mass_pipa
 
-	}
-}
+			}
+		}
 
-console.log(mass_bag)
-console.log(op)
+		console.log(mass_bag)
+		console.log(op)
 		localStorage.clear('a')
 		localStorage.setItem('a',JSON.stringify(mass_bag))
 
