2ac723fc7f539f6d29a9d7486751afd460d346c0
Stas <atlantisscale@gmail.com>
2023-06-19 21:20:21 +0300

    появление модального окна

--- public/JS/modal_bag.js
+++ public/JS/modal_bag.js
@@ -1,1 +1,22 @@
-modal_bag.js
+// // var myModal = document.querySelector('.modal_window')
+// // // myModalEl.addEventListener('show.bs.modal', function (event) {
+  
+// // // })
+
+// var myModalEl = document.querySelector('.modal_window')
+
+
+// // document.querySelector('.mod_window').onclick=()=>{
+  
+// //   // myModalEl.addEventListener('hidden.bs.modal', function (event) {
+// //   console.log('pipa')
+// // }
+// // })
+// // }
+
+// document.querySelector('.mod_window').onclick=()=>{
+//   // myModalEl.hide()
+//   // console.log('vdood')
+// }
+
+// // console.log(document.querySelector('.mod_window'))
