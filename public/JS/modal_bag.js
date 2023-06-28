78e8342b3438ba56013a77effdf9fbd7930980aa
Stas <atlantisscale@gmail.com>
2023-06-28 19:26:05 +0300

    начало 29.06.23

--- public/JS/modal_bag.js
+++ public/JS/modal_bag.js
@@ -2,34 +2,39 @@ document.querySelector('.bag').onclick=function(){
   let num_price=1
 
 
- let mass_bag_win=localStorage.getItem('a')
-    mass_bag_win=JSON.parse(mass_bag_win)
-    // удаление чтобы небыло накопления
-    let mass_bag_sorage=document.querySelectorAll('.kook')
-    console.log(mass_bag_sorage)
+  let mass_bag_win=localStorage.getItem('a')
+  mass_bag_win=JSON.parse(mass_bag_win)
+  // удаление чтобы небыло накопления
+  let mass_bag_sorage=document.querySelectorAll('.kook')
+  console.log(mass_bag_sorage)
 
   for (i of mass_bag_sorage) {
     i.remove()
   }
 
-for (let tot of mass_bag_win) {
+  for (let tot of mass_bag_win) {
 
-let div = document.createElement('div');
-div.className='kook'
-div.innerHTML=`<div class="container">
-  <!-- <div class="row"> -->
-    <div class="row">
-      <div class="col-md-4"><img src=${tot.img} class="rounded mx-auto d-block" alt="..."></div>
-      <div class="col-md-6" style="background-color: #F8F9FA;"><h5 class="">${tot.name}</h5></div>
-      <div class="col-md-2"><input type="number" min="1" max="15" class="input_fild" style="width:50px;"><h3><h3>${tot.cost}</h3>руб</h3></div>
-    </div>
+    let div = document.createElement('div');
+    div.className='kook'
+    div.innerHTML=`<div class="container">
+      <!-- <div class="row"> -->
+        <div class="row">
+          <div class="col-md-4"><img src=${tot.img} class="rounded mx-auto d-block" alt="..."></div>
+          <div class="col-md-6" style="background-color: #F8F9FA;"><h5 class="">${tot.name}</h5></div>
+          <div class="col-md-2"><input type="number" min="1" max="15" class="input_fild" style="width:50px;"><h3><h3>${tot.cost}</h3>руб</h3>
+            <div class="row">
+              <div class="col-md"></div>
+              <div class="col-md"><button data-id="{{this.post_id}}" class="btn btn-danger">Удалить из корзины</button></div>
+              <div class="col-md"></div>
+            </div>
 
-    <div class="row">
-      <div class="col-md"></div>
-      <div class="col-md"><button data-id="{{this.post_id}}" class="btn btn-danger">Удалить из корзины</button></div>
-      <div class="col-md"></div>
-    </div>
-    
+
+
+          </div>
+          <div class="col-md-12" style="height: 25px;" ></div>
+        </div>
+
+
 <!--     <div class="col">
       <img src=${tot.img} class="rounded mx-auto d-block" alt="...">
     </d
@@ -40,7 +45,7 @@ div.innerHTML=`<div class="container">
       <h3>${tot.cost}</h3>руб</p>
     </div>
   </div> -->
-<!-- </div> -->
+  <!-- </div> -->
 
 
 
@@ -79,17 +84,17 @@ div.innerHTML=`<div class="container">
 
 </div> -->
 
-    <!-- </li> -->
-    
-    
-  <!-- </ul> -->
+<!-- </li> -->
+
+
+<!-- </ul> -->
  <!--  <div class="card-body">
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
   </div> -->
 </div>`
 
-  document.querySelector('.img_product').append(div); 
+document.querySelector('.img_product').append(div); 
 
 
 let input_fild=document.querySelectorAll('.input_fild')
